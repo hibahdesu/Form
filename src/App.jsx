e26a72fdba777b0e4c37054ad4d561913f54b5e8
hibahdesu <hibahsindi@gmail.com>
2025-06-21 20:40:10 +0300

    Update the context in the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css';
 import Form from './Form';
 import UserContext from './contexts/UserContext';
 
+
 function App() {
   return (
     <UserContext.Provider value={{userName: "JohnDoe", name: "John Doe", email: "Johne@gmail.com", isAuthenticated: true}}>
       <div className="App">
-        <Form title={'Form'}/>
+        <Form title={`Welcome`}/>
       </div>
     </UserContext.Provider>
   );
